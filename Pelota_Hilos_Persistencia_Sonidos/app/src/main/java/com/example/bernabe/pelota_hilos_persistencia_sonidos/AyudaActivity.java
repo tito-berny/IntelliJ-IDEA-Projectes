7dba73a781c7b78c2c4904c0f2de313f4b1aef7d
Bernabé <bernabe@iMac-de-visibletic.local>
2018-01-18 13:17:39 +0100

    Inicio Juego pelota

---
+++ Pelota_Hilos_Persistencia_Sonidos/app/src/main/java/com/example/bernabe/pelota_hilos_persistencia_sonidos/AyudaActivity.java
@@ -0,0 +1,19 @@
+package com.example.bernabe.pelota_hilos_persistencia_sonidos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AyudaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda);
+    }
+
+    public void volver (View view){
+
+        onBackPressed();
+    }
+}
---
+++ Pelota_Hilos_Persistencia_Sonidos/app/src/main/java/com/example/bernabe/pelota_hilos_persistencia_sonidos/Partida.java
@@ -0,0 +1,144 @@
+package com.example.bernabe.pelota_hilos_persistencia_sonidos;
+
+/**
+ * Created by bernabe on 18/1/18.
+ */
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.content.ContextCompat;
+import android.view.Display;
+import android.view.WindowManager;
+
+
+public class Partida extends android.support.v7.widget.AppCompatImageView {
+
+    private int acel;
+    private Bitmap pelota, fondo;
+    private int tam_pantX, tam_pantY, posX, posY, velX, velY;
+    private int tamPelota;
+    boolean pelota_sube;
+
+    /**
+     * Crea el contexto, inicializa la posicion de la pelota y determina la dificultad
+     * @param contexto Para construir la nueva vista
+     * @param nivel_dificultad Determina el nivel de dificultat augmentando la aceleracion
+     */
+    public Partida(Context contexto, int nivel_dificultad){
+
+        super(contexto);
+
+        WindowManager manejador_ventana=(WindowManager) contexto.getSystemService(Context.WINDOW_SERVICE);
+
+        Display pantalla=manejador_ventana.getDefaultDisplay();
+
+        Point maneja_coord=new Point();   //Integra 2 coordenadas X,Y
+
+        pantalla.getSize(maneja_coord);   //Determina el tamaño de la pantalla donde se ejecuta la app
+
+        tam_pantX=maneja_coord.x;         //Establece el tamaño a las variables
+
+        tam_pantY=maneja_coord.y;
+
+        //Construye un layaut programatico () sobre la imagen que le pasamos
+        BitmapDrawable dibujo_fondo=(BitmapDrawable) ContextCompat.getDrawable(contexto, R.drawable.paisaje_1);
+
+        fondo=dibujo_fondo.getBitmap();// mirar en api getBitmap en clase BitmapDrawable. esto nos lleva a la siguiente instr.
+
+        fondo=Bitmap.createScaledBitmap(fondo, tam_pantX, tam_pantY, false);//mirar en clase Bitmap
+
+
+        BitmapDrawable objetoPelota=(BitmapDrawable)ContextCompat.getDrawable(contexto, R.drawable.pelota_11);
+
+        pelota=objetoPelota.getBitmap();
+
+        //Define el tamaño de la pelota con respecto a la pantalla
+        tamPelota=tam_pantY/3;
+
+        pelota=Bitmap.createScaledBitmap(pelota, tamPelota, tamPelota, false);
+
+        //Coocamos la pelota en el centro de la pantalla
+        posX=tam_pantX/2-tamPelota/2;
+        //Colocamos la pelota justo encima de la pantalla(por eso no se ve)
+        posY=0-tamPelota;
+
+        //Segun la dificultat augmentara la velocidad
+        acel=nivel_dificultad*(maneja_coord.y/400);
+
+
+    }
+
+    /**
+     * Clase que controla el toque en la pantalla sobre la pelota
+     * @param x Recive cordenada del toque en la pantalla
+     * @param y Recive cordenada del toque en la pantalla
+     * @return  Por defecto true, false si noo cumple los requisitos
+     */
+    public boolean toque(int x, int y){
+
+        if(y<tam_pantY/3) return false;
+
+        if(velY<=0) return false;
+
+        if(x<posX || x> posX+tamPelota) return false;
+
+        if(y<posY || y>posY+tamPelota) return false;
+
+        velY=-velY;
+
+        double desplX=x-(posX+tamPelota/2);
+
+        desplX=desplX/(tamPelota/2)*velY/2;
+
+        velX+=(int)desplX;
+
+        return true;
+    }
+
+    /**
+     * Clase que controla el calculo que determina el movimiento de la pelota
+     * @return Boolean por defecto false, sera true cuando cumpla los requisitos
+     */
+    public boolean movimientoBola(){
+
+        if(posX<0-tamPelota){
+
+            posY=0-tamPelota;
+
+            velY=acel;
+        }
+
+        posX+=velX;
+
+        posY+=velY;
+
+        if(posY>=tam_pantY) return true;
+
+        if(posX+tamPelota<0 || posX>tam_pantX) return true;
+
+        if(velY<0) pelota_sube=true;
+
+        if(velY>0 && pelota_sube){
+
+            pelota_sube=false;
+
+        }
+
+        velY+=acel;
+
+        return false;
+    }
+
+    protected void onDraw(Canvas lienzo){
+
+        lienzo.drawBitmap(fondo, 0,0, null);
+
+        lienzo.drawBitmap(pelota, posX, posY, null);
+
+
+    }
+}
+
