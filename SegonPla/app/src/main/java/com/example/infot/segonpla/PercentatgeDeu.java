ea79228e807831526e0db2c0fbf057c25d6bbddd
berny <berny_tito@hotmail.com>
2018-03-01 20:04:04 +0100

    SegundoPlano asyncTask

---
+++ SegonPla/app/src/main/java/com/example/infot/segonpla/PercentatgeDeu.java
@@ -0,0 +1,47 @@
+package com.example.infot.segonpla;
+
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
+
+/**
+ * Created by infot on 01/03/18.
+ */
+
+public class PercentatgeDeu extends AsyncTask <Integer, Integer, String>{
+
+    private int tiempo = 1000;
+    private ProgressBar progressBar1;
+
+
+    public PercentatgeDeu(ProgressBar progressBar1) {
+        this.progressBar1 = progressBar1;
+    }
+
+    @Override
+    protected String doInBackground(Integer... integers) {
+
+        for (int i = 0; i < 100; i++) {
+
+            try {
+
+                Thread.sleep(tiempo);
+                publishProgress(i);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "Task Completed.";
+
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+
+        progressBar1.setProgress(values[0]);
+    }
+
+
+
+}
