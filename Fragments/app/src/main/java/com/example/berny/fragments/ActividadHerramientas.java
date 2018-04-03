26f98d1f80c2dcf1ab707801e794a9dec961d975
titoberny <berny_tito@hotmail.com>
2018-04-03 20:28:58 +0200

    fragments1

---
+++ Fragments/app/src/main/java/com/example/berny/fragments/ActividadHerramientas.java
@@ -0,0 +1,13 @@
+package com.example.berny.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadHerramientas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_herramientas);
+    }
+}
--- Fragments/app/src/main/java/com/example/berny/fragments/menu.java
+++ Fragments/app/src/main/java/com/example/berny/fragments/menu.java
@@ -1,21 +1,24 @@
 package com.example.berny.fragments;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class menu extends Fragment {
+public class Menu extends Fragment {
 
+    private final int[] BOTONESMENU = {R.id.linterna, R.id.musica, R.id.nivel};
 
-    public menu() {
-        // Required empty public constructor
+    public Menu() {
+
     }
 
 
@@ -23,7 +26,34 @@ public class menu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        View mimenu = inflater.inflate(R.layout.fragment_menu, container, false);
+
+
+        ImageButton botonmenu;
+
+        for (int i = 0 ; i<BOTONESMENU.length; i++){
+
+            //Hacemos un casting de las imagenes en los botones
+            botonmenu = (ImageButton) mimenu.findViewById(BOTONESMENU[i]);
+
+            final int queBoton = i;
+
+            botonmenu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    //Guadamos en la variable la actividad donde estamos
+                    Activity estaActividad = getActivity();
+
+                    //Hacemos un casting de estaActividad y la convertimos en ComunicaMenu
+                    ((ComunicaMenu)estaActividad).menu(queBoton);
+
+                }
+            });
+        }
+
+
+        return mimenu;
     }
 
 }
